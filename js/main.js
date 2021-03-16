84bf6bcee99e5700f0b01e943354dcc57ff6d2ff
Tardelle720 <tardeller720@gmail.com>
2021-03-16 10:06:47 -0300

    adicionando arquivos

---
+++ js/main.js
@@ -0,0 +1,35 @@
+function soma(n1, n2){
+    return n1 + n2;
+   
+}
+
+function setReplace(frase, nome)
+frase.replace("nome, novo_nome")
+
+alert(soma(5,10));
+alert("vai japão, japão, Brasil")
+
+
+/*var count;
+for(count=1; count <+5; count++) {
+alert(count);
+}
+
+
+
+/*var count = 0;
+while(count <= 5){
+    console.log(count);
+    alert(count);
+    count++
+}*/
+
+
+
+/*var idade = prompt("qual sua idade?");
+
+if(idade >= 18) {
+     alert("maior de idade")
+}else {
+    alert("menor de idade")
+}*/
